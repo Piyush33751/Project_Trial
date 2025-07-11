23aefdac41e78cfb9451107ba568485d5e07d6a5
sue-e <suee.24@ichat.sp.edu.sg>
2025-07-11 20:50:16 +0800

    LCD code

--- src/main.py
+++ src/main.py
@@ -188,3 +188,12 @@ if __name__ == '__main__':
 
 
 
+lcd = LCD.lcd()
+lcd.lcd_clear()
+
+if(temp_humid_sensor < 45):
+    lcd.lcd_display_string("Have a Nice Day")
+elif(temp_humid_sensor >= 45 and temp_humid_sensor < 100):
+    lcd.lcd_display_string("There is fire, Exit here")
+elif(temp_humid_sensor >= 100):
+    lcd.lcd_display_string("There is fire, Evacuate to a safe location")
