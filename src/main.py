9b33684b03ddfb30764553e0bbe23b7b151f9eb3
piyush <PIYUSH72.24@ichat.sp.edu.sg>
2025-07-16 15:49:36 +0800

    buzzer branch

--- src/main.py
+++ src/main.py
@@ -187,13 +187,3 @@ if __name__ == '__main__':
     main()
 
 
-
-lcd = LCD.lcd()
-lcd.lcd_clear()
-
-if(temp_humid_sensor < 45):
-    lcd.lcd_display_string("Have a Nice Day")
-elif(temp_humid_sensor >= 45 and temp_humid_sensor < 100):
-    lcd.lcd_display_string("There is fire, Exit here")
-elif(temp_humid_sensor >= 100):
-    lcd.lcd_display_string("There is fire, Evacuate to a safe location")
